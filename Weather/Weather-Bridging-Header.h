14e2eddf2d147e35e394dc3532f047265ca5e1cd
ondra <ostocek@gmail.com>
2015-03-22 23:27:50 +0100

    added objective c bridging header

---
+++ Weather/Weather-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Weather-Bridging-Header.h
+//  Weather
+//
+//  Created by Ondřej Štoček on 22.03.15.
+//  Copyright (c) 2015 Ondrej Stocek. All rights reserved.
+//
+
+#ifndef Weather_Weather_Bridging_Header_h
+#define Weather_Weather_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
